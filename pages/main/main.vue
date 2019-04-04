e5c36afcc42e2cba5e749cefc6d96166c2758137
dongjianqiang <dongjianqiang@ann9.com>
2019-04-04 18:40:55 +0800

    inint

---
+++ pages/main/main.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content">
+		<!-- swiper -->
+		<view class="uni-padding-wrap">
+			<view class="page-section swiper">
+				<view class="page-section-spacing">
+					<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+						<swiper-item>
+							<view class="swiper-item uni-bg-red">A</view>
+						</swiper-item>
+						<swiper-item>
+							<view class="swiper-item uni-bg-green">B</view>
+						</swiper-item>
+						<swiper-item>
+							<view class="swiper-item uni-bg-blue">C</view>
+						</swiper-item>
+					</swiper>
+				</view>
+			</view>
+		</view>
+		<!-- grid -->
+		<uni-grid :options="[
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/shu.png',text:'院校库'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/lindang.png',text:'专业库'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/laoren.png',text:'排名库'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/laoren.png',text:'留学资讯'},
+			{image:'https://img-cdn-qiniu.dcloud.net.cn/img/liwu.png',text:'问答'},]"
+		 column-num="5">
+		</uni-grid>
+	</view>
+</template>
+
+<script>
+	import {
+		uniGrid
+	} from '@dcloudio/uni-ui'
+
+	export default {
+		data() {
+			return {
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
+			}
+		},
+		components: {
+			uniGrid
+		},
+		onLoad() {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,101 @@
+<template>
+    <view class="content">
+		<view v-if="hasLogin" class="hello">
+			<view class="title">
+				您好 {{userName}}，您已成功登录。
+			</view>
+		</view>
+		<view v-if="!hasLogin" class="hello">
+			<view class="title">
+				您好 游客。
+			</view>
+		</view>
+        <view class="btn-row">
+            <button v-if="!hasLogin" type="primary" class="primary" @tap="bindLogin">登录</button>
+            <button v-if="hasLogin" type="default" @tap="bindLogout">退出登录</button>
+        </view>
+    </view>
+</template>
+
+<script>
+    import {
+        mapState,
+        mapMutations
+    } from 'vuex'
+
+    export default {
+        computed: {
+            ...mapState(['hasLogin', 'forcedLogin','forcedLogin'])
+        },
+        methods: {
+            ...mapMutations(['logout']),
+            bindLogin() {
+                uni.navigateTo({
+                    url: '../login/login',
+                });
+            },
+            bindLogout() {
+                this.logout();
+                /**
+                 * 如果需要强制登录跳转回登录页面
+                 */
+                if (this.forcedLogin) {
+                    uni.reLaunch({
+                        url: '../login/login',
+                    });
+                }
+            }
+        },
+		onLoad(){
+			if (!this.hasLogin) {
+				uni.showModal({
+					title: '未登录',
+					content: '您未登录，需要登录后才能继续',
+					/**
+					 * 如果需要强制登录，不显示取消按钮
+					 */
+					showCancel: !this.forcedLogin,
+					success: (res) => {
+						if (res.confirm) {
+							/**
+							 * 如果需要强制登录，使用reLaunch方式
+							 */
+							if (this.forcedLogin) {
+								uni.reLaunch({
+									url: '../login/login'
+								});
+							} else {
+								uni.navigateTo({
+									url: '../login/login'
+								});
+							}
+						}
+					}
+				});
+			}
+		}
+    }
+</script>
+
+<style>
+.hello {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+	}
+
+	.title {
+		color: #8f8f94;
+		margin-top: 50upx;
+	}
+
+	.ul {
+		font-size: 30upx;
+		color: #8f8f94;
+		margin-top: 50upx;
+	}
+
+	.ul>view {
+		line-height: 50upx;
+	}
+</style>
